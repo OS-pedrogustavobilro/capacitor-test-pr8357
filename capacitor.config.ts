90958dc8e1f956343e86c6710205a1f1edb66285
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-02-25 13:25:54 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capacitor-test-pr5956',
+  webDir: 'dist'
+};
+
+export default config;
